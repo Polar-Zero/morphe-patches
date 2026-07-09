35871787dd6b97375693c85432d4bafb1448c102
PolarZero <trinity5000@naver.com>
2026-07-09 19:09:14 +0900

    5

--- extensions/blackroom_gm/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-extension {
-    name = "extensions/extension.mpe"
-}
-
-android {
-    namespace = "com.devblackroom.blanc.gm"
-}
--- patches/src/main/kotlin/app/blackroom_gm/SavableVersionPatch.kt
+++ patches/src/main/kotlin/app/blackroom_gm/SavableVersionPatch.kt
@@ -1,19 +1,29 @@
-package app.template.patches.blackroom_gm
+package app.blackroom_gm
 
-import app.morphe.patcher.PatcherContext
+import app.morphe.patcher.patch.ApkFileType
+import app.morphe.patcher.patch.AppTarget
+import app.morphe.patcher.patch.Compatibility
 import app.morphe.patcher.patch.rawResourcePatch
 import java.io.File
 import java.nio.file.Files
-import app.template.patches.shared.Constants.COMPATIBILITY_BLACKROOM_GM
-/*
-fun addFile(destPath: String, srcFile: File, packageName: String): File {
-	//Jar();
-	return null;
-}*/
+
+@Suppress("unused")
+val COMPATIBILITY_TARGET = Compatibility(
+	name = "길드 마스터가 되어주세요!",
+	packageName = "com.devblackroom.blanc.gm",
+	apkFileType = ApkFileType.APKS,
+	appIconColor = 0xFF0045,
+	targets = listOf(
+		AppTarget(
+			version = "2.1.8"
+		)
+	)
+)
+
 @Suppress("unused")
 val savableVersionPatch = rawResourcePatch(name="저장가능 버전 패치",description = "오프라인 진행 및 저장 기능을 활성화합니다.",default = true)
 {
-	compatibleWith(COMPATIBILITY_BLACKROOM_GM)
+	compatibleWith(COMPATIBILITY_TARGET)
 	execute()
 	{
 		delete("res/values/strings.xml")
