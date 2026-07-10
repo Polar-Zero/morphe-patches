2dd4060b622152a53bb110ac35c6ff0b57e82204
PolarZero <trinity5000@naver.com>
2026-07-11 01:30:26 +0900

    feat: Released Blackroom GuildMaster Patch

--- patches/src/main/kotlin/blackroom_gm/SavableVersionPatch.kt
+++ patches/src/main/kotlin/blackroom_gm/SavableVersionPatch.kt
@@ -8,6 +8,8 @@ import app.morphe.patcher.patch.rawResourcePatch
 import app.morphe.patcher.patch.resourcePatch
 import jdk.javadoc.internal.tool.Main.execute
 import java.util.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+import kotlin.io.encoding.decodingWith
 
 @Suppress("unused")
 val COMPATIBILITY_TARGET = Compatibility(
@@ -22,15 +24,22 @@ val COMPATIBILITY_TARGET = Compatibility(
 	)
 )
 
+@OptIn(ExperimentalEncodingApi::class)
 @Suppress("unused")
 val savableVersionPatch = rawResourcePatch(name="저장가능 버전 패치",description = "오프라인 진행 및 로컬 저장 기능을 활성화합니다.",default = true)
 {
 	compatibleWith(COMPATIBILITY_TARGET)
 	execute()
 	{
-		val fileName = Base64.getDecoder().decode("YXNzZXRzL2Jpbi9EYXRhL01hbmFnZWQvQXNzZW1ibHktQ1NoYXJwLmRsbA==").toString(Charsets.UTF_8);
+		val baseFolder = Base64.getDecoder().decode("YXNzZXRzL2Jpbi9EYXRhL01hbmFnZWQv").toString(Charsets.UTF_8);
+		get(baseFolder+Base64.getDecoder().decode("UHVyY2hhc2luZy5Db21tb24uZGxs").toString(Charsets.UTF_8)).delete();
+		get(baseFolder+Base64.getDecoder().decode("VGFwam95LkFuZHJvaWQuZGxs").toString(Charsets.UTF_8)).delete();
+		get(baseFolder+Base64.getDecoder().decode("VGFwam95LmRsbA").toString(Charsets.UTF_8)).delete();
+		get(baseFolder+Base64.getDecoder().decode("VGl6ZW4uZGxs").toString(Charsets.UTF_8)).delete();
+		val fileName = baseFolder+Base64.getDecoder().decode("QXNzZW1ibHktQ1NoYXJwLmRsbA==").toString(Charsets.UTF_8);
 		val dest = get(fileName);
 		val newfile = inputStreamFromBundledResource("blackroom_gm", "Data.bin");
+		//newfile.decodingWith(base64 = kotlin.io.encoding.Base64(false, true, true, true))
 		
 		dest.writeBytes(newfile!!.readAllBytes())
 	}
