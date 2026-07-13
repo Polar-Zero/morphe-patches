3f2434ab23ad4cff770e1739337e6d4f3d7651e5
PolarZero <trinity5000@naver.com>
2026-07-13 21:09:55 +0900

    feat: 흡혈귀의 연애방법 테스트 패치

---
+++ patches/src/main/kotlin/blackroom_vp/SDKTargetVersionPatch.kt
@@ -0,0 +1,53 @@
+package blackroom_vp
+
+import app.morphe.patcher.patch.ApkFileType
+import app.morphe.patcher.patch.AppTarget
+import app.morphe.patcher.patch.Compatibility
+import app.morphe.patcher.patch.resourcePatch
+import org.w3c.dom.Element
+
+
+@Suppress("unused")
+val COMPATIBILITY_TARGET = Compatibility(
+	name = "흡혈귀의 연애방법",
+	packageName = "info.blackroom.vampirelovemain.renew",
+	apkFileType = ApkFileType.APK,
+	appIconColor = 0xFF0045,
+	targets = listOf(
+		AppTarget(
+			version = "1.6.2"
+		)
+	)
+)
+
+@Suppress("unused")
+val mainPatch = resourcePatch(name="SDK 버전 변경",description = "대상 SDK를 강제로 변경하여 Android 12~14 기기에서 설치 가능하도록 조정합니다. (갤23, 폴드/플립5 등)\n64Bit만 지원하는 기기에는 여전히 설치가 어렵습니다 (갤24+, 폴드/플립6+ 등 불가)",default = true)
+{
+	compatibleWith(COMPATIBILITY_TARGET)
+	execute()
+	{
+		document("AndroidManifest.xml").use { d ->
+			var i = 0
+			while(i<d.getElementsByTagName("manifest").length)
+			{
+				val x = d.getElementsByTagName("manifest").item(i) as? Element
+				if(x!=null)
+				{
+					if(x.getAttribute("platformBuildVersionCode")!=null)
+					{
+						x.setAttribute("platformBuildVersionCode", "32")
+					}
+					if(x.getAttribute("platformBuildVersionName")!=null)
+					{
+						x.setAttribute("platformBuildVersionName", "12")
+					}
+					val n = d.createElement("uses-sdk")
+					n.setAttribute("android:minSdkVersion", "26")
+					n.setAttribute("android:targetSdkVersion", "35")
+					x.appendChild(n)
+				}
+				i++
+			}
+		}
+	}
+}
