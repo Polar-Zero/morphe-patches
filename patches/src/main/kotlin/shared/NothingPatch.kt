779ed106fbdf4d323cea479f4ecea3ae037638ce
PolarZero <trinity5000@naver.com>
2026-07-14 01:41:45 +0900

    feat: Add Nothing Patch #3

--- patches/src/main/kotlin/shared/NothingPatch.kt
+++ patches/src/main/kotlin/shared/NothingPatch.kt
@@ -3,12 +3,12 @@ package shared;
 import app.morphe.patcher.patch.ApkFileType
 import app.morphe.patcher.patch.AppTarget
 import app.morphe.patcher.patch.Compatibility
-import app.morphe.patcher.patch.resourcePatch
+import app.morphe.patcher.patch.bytecodePatch
 import org.w3c.dom.Element
 
 //Code from https://github.com/rushiranpise/morphe-patches/blob/main/patches/src/main/kotlin/app/template/patches/shared/universal/UniversalPatches.kt#L161
 @Suppress("unused")
-val NothingPatch = resourcePatch(name="더미 패치",description = "이 패치는 아무 코드도 실행하지 않습니다. apk 서명을 변경해야 할 때 유용할지도?")
+val NothingPatch = bytecodePatch(name="더미 패치",description = "이 패치는 아무 코드도 실행하지 않습니다. apk 서명을 변경해야 할 때 유용할지도?",default=false)
 {
 	execute()
 	{
